b165e8ccad9ac227217131bf1b5ef35add68e057
Gondnat <julykitt@gmail.com>
2019-06-12 14:54:04 +0800

    add check for result for make publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 make publish
+if [ $? == 0 ]; then
 echo "hardcode.today" > ./output/CNAME
 ghp-import output
 git push -f git@github.com:Gondnat/gnodnate.github.io.git gh-pages:master
+fi
